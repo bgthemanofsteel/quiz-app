3c5567f0fc902c31e2a90824171146b351f1183a
Brandon Gregory <bgthemanofsteel@gmail.com>
2019-02-18 16:38:43 -0600

    added placeholder functions

---
+++ index.js
@@ -0,0 +1,40 @@
+let questionNumber = 0;
+let score = 0;
+
+function handleQuizStart(){
+    $('.quizStart').on('click', '.startButton' function(event)){
+        handleQuestionCount(); 
+        $('.quizStart').remove();
+    }
+    
+};
+
+function handleQuestionCount(){
+    questionNumber ++;
+    $('.questionNumber').text(questionNumber+1);
+    
+};
+
+function handleScoreCount(){
+    $('.questionAnswerForm').on('click', '.')
+);
+
+function handleStart(){
+
+};
+
+function handleAnswers(){
+
+};
+
+function handleRightWrongMessage(){
+
+};
+
+function handleEndMessage(){
+
+};
+
+function handleScore(){
+
+};
---
+++ store.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+        question: 'Bruce Wayne is which Superhero?',
+        choices: [
+            'Spiderman',
+            'Batman',
+            'Aquaman',
+            'Antman'
+            ],
+        correctAnswer: 'Batman',
+        image: 'https://1000logos.net/wp-content/uploads/2016/10/batman-logo-2000.jpg',
+        alt: 'Batman logo'
+    },
+    {
+        question: 'Steve Rogers is which Superhero?',
+        choices: [
+            'Captain America',
+            'Green Arrow',
+            'Superman',
+            'Wolverine'
+            ],
+        correctAnswer: 'Captain America',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61G2USZC%2BNL._UX522_.jpg',
+        alt: 'Captain America logo'
+    },
+    {
+        question: "Spiderman's real name is?",
+        choices: [
+            'Bruce Wayne',
+            'Lance Gallant',
+            'Clark Kent',
+            'Peter Parker'
+            ],
+        correctAnswer: 'Peter Parker',
+        image: 'https://i.pinimg.com/originals/7f/f7/d9/7ff7d93eaa14a904e3e35013fb7550fd.png',
+        alt: 'Spiderman logo'
+    },
+    {
+        question: "Cyclop's real identity is:",
+        choices: [
+            'Scott Summers',
+            'Peter Parker',
+            'Jimmy Olson',
+            'Barry Allen'
+            ],
+        correctAnswer: 'Scott Summers',
+        image: 'https://i.pinimg.com/originals/80/bb/f6/80bbf63e6ddeba8297c280e13c9bb2eb.jpg',
+        alt: 'Cyclops logo'
+    },
+    {
+        question: 'Oliver Queen is which Superhero?',
+        choices: [
+            'Green Arrow',
+            'Green Lantern',
+            'Martian Manhunter',
+            'Captain Marvel'
+            ],
+        correctAnswer: 'Green Arrow',
+        image: 'https://vignette.wikia.nocookie.net/logopedia/images/6/6e/Arrow_season_5_title_card.png/revision/latest?cb=20171004002943',
+        alt: 'Green Arrow logo'
+    },
+    {
+        question: "Wolverine's real name is:",
+        choices: [
+            'Logan',
+            'Brandon',
+            'Scott',
+            'Oliver'
+            ],
+        correctAnswer: 'Logan',
+        image: 'https://res.cloudinary.com/teepublic/image/private/s--kV9ORmCf--/t_Preview/b_rgb:ffb81c,c_limit,f_auto,h_313,q_90,w_313/v1530925012/production/designs/2864742_0',
+        alt: 'Wolverine logo'
+    },
+    {
+        question: "The Flash's real identity is:",
+        choices: [
+            'Barry Allen',
+            'Peter Parker',
+            'Clark Kent',
+            'John Jones'
+            ],
+        correctAnswer: 'Barry Allen',
+        image: 'https://res.cloudinary.com/teepublic/image/private/s--qGKEnqg8--/t_Preview/b_rgb:c62b29,c_limit,f_jpg,h_630,q_90,w_630/v1449793529/production/designs/369093_1.jpg',
+        alt: 'The Flash logo'
+    },
+    {
+        question: 'Clark Kent is which Superhero?',
+        choices: [
+            'Batman',
+            'Thor',
+            'Ironman',
+            'Superman'
+            ],
+        correctAnswer: 'Superman',
+        image: 'https://images.esellerpro.com/2486/I/145/4/u_20260840_Half%20Moon%20Bay_Homewares_Coasters_coaster-superman-logo.JPG',
+        alt: 'Superman logo'
+    },
+    {
+        question: "What is Wonder Woman's real name?",
+        choices: [
+            'Natacia Romanov',
+            'Kara Zor-El',
+            'Diana Prince',
+            'Carol Danvers'
+            ],
+        correctAnswer: 'Diana Prince',
+        image: 'http://assets.suredone.com/1868/media-photos/sd3195-wonder-woman-logo-fridge-magnet-dc-comics-justice-league-comic-book-hero.jpeg',
+        alt: 'Wonder Woman logo'
+    },
+    {
+        question: "Captain Marvel's real name is:",
+        choices: [
+            'Steve Rogers',
+            'Bruce Banner',
+            'Carol Danvers',
+            'Tony Stark'
+            ],
+        correctAnswer: 'Carol Danvers',
+        image: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/captain-marvel-logo-retno-musyakimah.jpg',
+        alt: 'Captain Mavel logo'
+    },
+]
