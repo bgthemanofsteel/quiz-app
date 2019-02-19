50271cd63bbec86389757df9824295460cd18fac
Brandon Gregory <bgthemanofsteel@gmail.com>
2019-02-18 20:45:32 -0600

    added quiz section to wireframe

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-let questionNumber = 0;
-let score = 0;
+// let questionNumber = 0;
+// let score = 0;
 
-function handleQuizStart(){
-    $('.quizStart').on('click', '.startButton' function(event)){
-        handleQuestionCount(); 
-        $('.quizStart').remove();
-        $('.questionNumber').text(1);
-    }
-};
+// function handleQuizStart(){
+//     $('.quizStart').on('click', '.startButton' function(event)){
+//         handleQuestionCount(); 
+//         $('.quizStart').remove();
+//         $('.questionNumber').text(1);
+//     }
+// };
 
-function handleQuestionCount(){
-    questionNumber ++;
-    $('.questionNumber').text(questionNumber+1);
-};
+// function handleQuestionCount(){
+//     questionNumber ++;
+//     $('.questionNumber').text(questionNumber+1);
+// };
 
-function handleScoreCount(){
-    $('.questionAnswerForm').on('click', '.')
-);
+// function handleScoreCount(){
+//     $('.questionAnswerForm').on('click', '.')
+// );
 
-function handleStart(){
+// function handleStart(){
 
-};
+// };
 
-function handleAnswers(){
+// function handleAnswers(){
 
-};
+// };
 
-function handleRightWrongMessage(){
+// function handleRightWrongMessage(){
 
-};
+// };
 
-function handleEndMessage(){
+// function handleEndMessage(){
 
-};
+// };
 
-function handleScore(){
+// function handleScore(){
 
-};
+// };
